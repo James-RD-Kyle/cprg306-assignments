b024d60efe9fa41afd6dd0b4a4d0e95caa042d87
James Kyle <james.kyle@edu.sait.ca>
2025-09-15 15:24:56 -0600

    Completed Steps 1 - 3 of week 2 assignment.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo () {
+    return (
+        <div>
+            <p>Name: James Kyle</p>
+            <Link href="https://github.com/James-RD-Kyle/cprg306-assignments">GitHub Repo</Link>
+        </div>
+    );
+}
