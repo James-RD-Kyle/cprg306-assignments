9ee416eb01cff063c3a8185f26ee7e84cc2a8a96
James Kyle <james.kyle@edu.sait.ca>
2025-09-16 14:33:08 -0600

    Added an underline class to the Github Link

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -6,7 +6,10 @@ export default function StudentInfo () {
             <p>Name: James Kyle</p>
             <span>
                 Github: {" "} 
-                <Link href="https://github.com/James-RD-Kyle/cprg306-assignments" target="_blank">James-RD-Kyle/cprg306-assignments</Link>
+                <Link href="https://github.com/James-RD-Kyle/cprg306-assignments" 
+                target="_blank" 
+                className = "underline"
+                >James-RD-Kyle/cprg306-assignments</Link>
                 </span>
         </div>
     );
