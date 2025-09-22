992558fe8186f4646f354b0c5550f79a4df51f48
James Kyle <james.kyle@edu.sait.ca>
2025-09-22 14:36:37 -0600

    Created week-3 folder, item-list.js, item.js and page.js. Haven't been able to finish

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <header>
       <h1>CPRG 306: Web Development - Assignments</h1>
       <Link href="/week-2">Link to Week 2 →</Link>
+      <Link href="/week-3">Link to Week 3 →</Link>
     </header>
   );
 }
---
+++ app/week-3/item.js
@@ -0,0 +1,10 @@
+
+export function Item({ name, quantity, category }) { 
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Quantity: {quantity}</p>
+      <p>Category: {category}</p>
+    </div>
+  );
+}
