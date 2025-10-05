0e4bf4d0f336d36d00bb16464a608e65148ba1ff
James Kyle <james.kyle@edu.sait.ca>
2025-10-05 14:31:24 -0600

    Added Week 4 link Added Week 4 page placeholder Added New Item component placeholder

---
+++ app/week-4/new-item.js
@@ -0,0 +1,9 @@
+
+export default function NewItem() {
+    return (
+        <div>
+            <h2>New Item Next.Js Component</h2>
+            <p>This is a placeholder for the New Item Next.Js component.</p>
+        </div>
+    )
+}
