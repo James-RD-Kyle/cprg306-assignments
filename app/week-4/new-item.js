93294c6e1e43247d88c1c6ede32c05c456b94b9e
James Kyle <james.kyle@edu.sait.ca>
2025-10-05 22:24:31 -0600

    Removed Placeholder Text Added increment button Added decrement button Added Styling to buttons and Text Added functionality to buttons

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -3,10 +3,24 @@
 import {useState} from "react";
 
 export default function NewItem() {
+    const [quantity, setQuantity] = useState(1);
+
+    const increment = () => {
+        if (quantity < 20) {
+        setQuantity(quantity + 1);
+        }
+    }
+    const decrement = () => {
+        if (quantity > 1) {
+            setQuantity(quantity - 1);
+        }
+    }
     return (
-        <div>
-            <h2>New Item Next.Js Component</h2>
-            <p>This is a placeholder for the New Item Next.Js component.</p>
+        <div className="border-2 bg-white text-black text-center rounded-lg p-4 m-2">
+            <p>Quantity: {quantity}</p>
+            <button className="bg-stone-400 text-white rounded w-10 h-10 m-1" onClick={decrement}>-</button>
+            <button className="bg-blue-500 text-white rounded w-10 h-10 m-1" onClick={increment}>+</button>
+            <p className="text-gray-400">Quantity must be between 1 and 20</p>
         </div>
     )
 }
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,8 +3,10 @@ import NewItem from './new-item';
 export default function Page() {
     return (
         <main>
-            <h1>Placeholder</h1>
+            <div className="grid place-items-center items-baseline-last flex-initial m-2">
+            <h1 className='text-2xl font-bold'>Add New Item</h1>
             <NewItem/>
+        </div>
         </main>
     )
 }
