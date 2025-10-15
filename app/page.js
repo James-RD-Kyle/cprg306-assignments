fcdfd27d7b744bb674a2dae05befc3d4de4a0cdb
James Kyle <james.kyle@edu.sait.ca>
2025-10-14 19:02:37 -0600

    Major changes to recently edited files: - Updated button classes to conditionally apply cursor styles - Added cursor-pointer class to select element - Changed default category state to lowercase - Redid the styling almost completely for a cleaner look - Added cursor-pointer class to submit button - Changed outer div's rounded class from rounded-4xl to rounded-3xl - And much more...

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
       <Link href="/week-2">Link to Week 2 →</Link>
       <Link href="/week-3">Link to Week 3 →</Link>
       <Link href="/week-4">Link to Week 4 →</Link>
+      <Link href="/week-5">Link to Week 5 →</Link>
     </header>
   );
 }
