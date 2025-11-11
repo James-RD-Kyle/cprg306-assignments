42428cf761fc97bca4e65d55d9b2242a95ddd282
James Kyle <james.kyle@edu.sait.ca>
2025-11-10 21:26:54 -0700

    Updated AuthContext with correct import path Made Week-9 page login clear Added sign-in prompt to shopping list page if user is not logged in

---
+++ app/week-9/page.js
@@ -0,0 +1,26 @@
+"use client";
+
+import { useUserAuth } from "../contexts/AuthContext";
+import Link from 'next/link';
+
+export default function Page() {
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+    if (user == null) {
+        return (
+            <div className="grid place-items-center h-screen">
+                <h1>Please sign in</h1>
+                <button type="button" onClick={gitHubSignIn}>Sign in with GitHub</button>
+            </div>
+        );
+    }
+    if (!user) {
+        return (
+            <div>
+                <h1>Welcome, {user.displayName}</h1>
+                <button type="button"><Link href="/week-9/shopping-list">Shopping List</Link></button>
+                <button type="button" onClick={firebaseSignOut}>Sign Out</button>
+            </div>
+        );
+    }
+};
--- app/week-9/shopping-list/page.js
+++ app/week-9/shopping-list/page.js
@@ -5,10 +5,13 @@ import NewItem from './new-item';
 import itemsData from './items.json';
 import MealIdeas from './meal-ideas';
 import { useState } from 'react';
+import { useUserAuth } from "../../contexts/AuthContext";
+import Link from 'next/link';
 
 export default function Page() {
     const [items, setItems] = useState(itemsData);
     const [selectedItemName, setSelectedItemName] = useState();
+    const {user} = useUserAuth();
 
     const handleAddItem = (newItem) => {
         setItems((prevItems) => [...prevItems, newItem]);
@@ -16,7 +19,15 @@ export default function Page() {
     const handleItemSelect = (item) => {
         setSelectedItemName(item.name.toLowerCase().split(',')[0].replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, "").trim());
     };
-
+    
+    if (!user) {
+        return (
+            <div className="grid place-items-center h-screen">
+                <h1>Please sign in to view the shopping list</h1>
+                <button type="button"><Link href="/week-9">Go to Sign In Page</Link></button>
+            </div>
+        );
+    }
     return (
         <main>
             <div className="grid place-items-center items-baseline-last m-2">
