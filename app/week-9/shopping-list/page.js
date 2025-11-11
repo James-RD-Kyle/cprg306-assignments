4289f92690a3abebc1d716fdbdeda25dd7192cab
James Kyle <james.kyle@edu.sait.ca>
2025-11-10 23:25:32 -0700

    Styling Changes

--- app/week-9/shopping-list/page.js
+++ app/week-9/shopping-list/page.js
@@ -11,7 +11,7 @@ import Link from 'next/link';
 export default function Page() {
     const [items, setItems] = useState(itemsData);
     const [selectedItemName, setSelectedItemName] = useState();
-    const {user} = useUserAuth();
+    const { user } = useUserAuth();
 
     const handleAddItem = (newItem) => {
         setItems((prevItems) => [...prevItems, newItem]);
@@ -19,12 +19,14 @@ export default function Page() {
     const handleItemSelect = (item) => {
         setSelectedItemName(item.name.toLowerCase().split(',')[0].replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, "").trim());
     };
-    
+
     if (!user) {
         return (
-            <div className="grid place-items-center h-screen">
-                <h1>Please sign in to view the shopping list</h1>
-                <button type="button"><Link href="/week-9">Go to Sign In Page</Link></button>
+            <div className="flex justify-center items-center flex-col bg-slate-900 gap-4 h-screen">
+                <div className="bg-indigo-900 p-8 rounded text-center">
+                    <h1>Please sign in to view the shopping list</h1>
+                    <button type="button" className="bg-blue-500 rounded py-1 px-2"><Link href="/week-9">Go to Sign In Page</Link></button>
+                </div>
             </div>
         );
     }
