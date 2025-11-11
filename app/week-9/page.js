40ba03cde12919cb55d108453c52d25655e55888
James Kyle <james.kyle@edu.sait.ca>
2025-11-10 23:19:55 -0700

    Made Some Styling Changes to Shopping-list Page

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -9,17 +9,23 @@ export default function Page() {
 
     if (!user) {
         return (
-            <div className="grid place-items-center h-screen">
-                <h1>Please sign in</h1>
-                <button type="button" onClick={gitHubSignIn}>Sign in with GitHub</button>
+            <div className="flex justify-center items-center flex-col bg-slate-900 gap-4 h-screen">
+                <div className="bg-indigo-900 p-8 rounded text-center">
+                    <h1>Please sign in</h1>
+                    <button type="button" className="bg-blue-500 rounded py-1 px-2" onClick={gitHubSignIn}>Sign in with GitHub</button>
+                </div>
             </div>
         );
     }
     return (
-        <div>
-            <h1>Welcome, {user.displayName}</h1>
-            <button type="button"><Link href="/week-9/shopping-list">Shopping List</Link></button>
-            <button type="button" onClick={firebaseSignOut}>Sign Out</button>
+        <div className="flex justify-center items-center flex-col bg-slate-900 gap-4 h-screen">
+            <div className="bg-indigo-900 p-8 rounded text-center">
+                <h1>Welcome, {user.displayName}</h1>
+                <div className="flex gap-8">
+                    <button type="button" className="bg-green-500 rounded py-1 px-2" ><Link href="/week-9/shopping-list">Shopping List</Link></button>
+                    <button type="button" className="bg-red-500 rounded py-1 px-2" onClick={firebaseSignOut}>Sign Out</button>
+                </div>
+            </div>
         </div>
     );
 };
